a121d11497ef696ac32085e58cba0e940d7e8ee2
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-05-07 16:57:22 -0400

    Add Search for Exsisting json, filter();

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ const restaurantTitle = document.querySelector('.restaurant-title');
 const rating = document.querySelector('.rating');
 const minPrice = document.querySelector('.price');
 const category = document.querySelector('.category');
+const inputSearch = document.querySelector('.input-search');
 
 let login = localStorage.getItem("gloDelivery");
 
@@ -240,6 +241,62 @@ function init() {
     cartButton.addEventListener("click", toggleModal);
     closed.addEventListener("click", toggleModal);
     // work with restaurant cards
+
+    inputSearch.addEventListener('keydown', function (event) {
+        if (event.keyCode === 13) {
+            const target = event.target;
+
+            const value = target.value.toLowerCase().trim();
+
+            target.value = '';
+
+            if (!value || value.length < 4) {
+                target.style.backgroundColor = 'pink';
+                setTimeout(function () {
+                    target.style.backgroundColor = '';
+                }, 2000);
+                return;
+            }
+
+            const goods = [];
+
+            getData("./db/partners.json")
+                .then(function (data) {
+                    const products = data.map(function (item) {
+                        return item.products;
+                    })
+
+
+                    products.forEach(function (product) {
+                        getData(`./db/${product}`)
+                            .then(function (data) {
+                                goods.push(...data);
+
+                                const searchGoods = goods.filter(function (item) {
+                                    return item.name.toLowerCase().includes(value);
+                                })
+
+                                cardsMenu.textContent = "";
+                                containerPromo.classList.add("hide");
+                                restaurants.classList.add("hide");
+                                menu.classList.remove("hide");
+
+                                restaurantTitle.textContent = 'Результат поиска';
+                                rating.textContent = '';
+                                minPrice.textContent = '';
+                                category.textContent = '';
+
+                                return searchGoods;
+                            })
+                            .then(function (data) {
+                                data.forEach(createCardGood);
+                            })
+                    });
+
+                })
+
+        };
+    });
 
     checkAuth();
 
